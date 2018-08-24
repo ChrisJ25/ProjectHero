bc889bb32fa9cadceb6acfb989c0db3bd4991264
RogueLeader <doctordisco2564@gmail.com>
2018-08-24 17:27:03 -0400

    More FAction stuff

--- includes/Factions.h
+++ includes/Factions.h
@@ -4,12 +4,15 @@
 class Faction {
     public:
     Faction(string Name);
-    ~Faction();
     int Affinity;
-    
+    bool Eliminated;
     private:
     enum Tech_Level { L1, L2, L3, L4 }; /*{Regular Weapons, Laser Weapons, Power Armor, Plasma Weapons}*/
     string FactionName;
     
-
 };
+
+Faction::Faction( string Name ) {
+    FactionName=Name;
+
+}
